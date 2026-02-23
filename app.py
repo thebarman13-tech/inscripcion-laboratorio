12c908f3d62ff45885eeb8622b5bda95148c3299
thebarman13-tech <thebarman13@gmail.com>
2026-02-23 15:31:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -291,5 +291,5 @@ def dashboard():
 # ---------------- MAIN ----------------
 if __name__=="__main__":
    import os
-port = int(os.environ.get("PORT", 1000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host="0.0.0.0", port=port)
